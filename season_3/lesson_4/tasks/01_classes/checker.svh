73c348b193e307798c09f91fab81176980ed84af
serge06 <chusserge@mail.ru>
2025-02-01 11:41:02 +0300

    Feat(lesson_4): Улучшение задания по рандомизации классов

--- season_3/lesson_4/tasks/01_classes/checker.svh
+++ season_3/lesson_4/tasks/01_classes/checker.svh
@@ -17,27 +17,27 @@ initial begin
     repeat(10000) begin
         cl_1 = new(); 
         if(!cl_1.randomize())
-            $error("Can't randomize my_class_1, check constraints!");
+            $fatal(1, "Can't randomize my_class_1, check constraints!");
         mbx_1.put(cl_1);
         cl_2 = new(); 
         if(!cl_2.randomize())
-            $error("Can't randomize my_class_2, check constraints!");
+            $fatal(1, "Can't randomize my_class_2, check constraints!");
         mbx_2.put(cl_2);
         cl_3 = new(); 
         if(!cl_3.randomize())
-            $error("Can't randomize my_class_3, check constraints!");
+            $fatal(1, "Can't randomize my_class_3, check constraints!");
         mbx_3.put(cl_3);
         cl_4 = new(); 
         if(!cl_4.randomize())
-            $error("Can't randomize my_class_4, check constraints!");
+            $fatal(1, "Can't randomize my_class_4, check constraints!");
         mbx_4.put(cl_4);
         cl_5 = new(); 
         if(!cl_5.randomize())
-            $error("Can't randomize my_class_5, check constraints!");
+            $fatal(1, "Can't randomize my_class_5, check constraints!");
         mbx_5.put(cl_5);
         cl_6 = new(); 
         if(!cl_6.randomize())
-            $error("Can't randomize my_class_6, check constraints!");
+            $fatal(1, "Can't randomize my_class_6, check constraints!");
         mbx_6.put(cl_6);
     end
 end
@@ -69,50 +69,48 @@ initial begin
 
         // 1
 
-        if( ! (cl_1.b > 0) ) begin
-            $error("my_class_1.b <= 0");
-            $display("my_class_1.b = %0d", cl_1.b);
+        if( !(cl_1.divident > 0) ) begin
+            $error("my_class_1.divident == 0");
+            $display("my_class_1.divident = %0d", cl_1.divident);
         end
         
-        if( ! (cl_1.a % cl_1.b == 0) ) begin
-            $error("my_class_1.a is not divisible by my_class_1.b");
-            $display("my_class_1.a = %0d", cl_1.a);
-            $display("my_class_1.b = %0d", cl_1.b);
+        if( !(cl_1.divident % cl_1.divider == 0) ) begin
+            $error("my_class_1.divident is not divisible by my_class_1.divider");
+            $display("my_class_1.divident = %0d", cl_1.divident);
+            $display("my_class_1.divider = %0d", cl_1.divider);
         end
 
-        if( ! (cl_1.a > cl_1.b) ) begin
-            $error("my_class_1.a <= my_class_1.b");
-            $display("my_class_1.a = %0d", cl_1.a);
-            $display("my_class_1.b = %0d", cl_1.b);
-        end
-
-        if( ! (cl_1.a + cl_1.b > 100) ) begin
-            $error("my_class_1.a + my_class_1.b <= 100");
-            $display("my_class_1.a = %0d", cl_1.a);
-            $display("my_class_1.b = %0d", cl_1.b);
+        if( !(cl_1.divident > cl_1.divider) ) begin
+            $error("my_class_1.divident <= my_class_1.divider");
+            $display("my_class_1.divident = %0d", cl_1.divident);
+            $display("my_class_1.divider = %0d", cl_1.divider);
         end
 
         // 2
 
-        if( cl_2.a == 0 && (cl_2.b != 100)) begin
-            $error("my_class_2.a == 0, but my_class_2.b != 100");
-            $display("my_class_2.a = %0d", cl_2.a);
-            $display("my_class_2.b = %0d", cl_2.b);
+        if( cl_2.data == 0 && (cl_2.addr != 128)) begin
+            $error("my_class_2.data == 0, but my_class_2.data != 128");
+            $display("my_class_2.data = %0d", cl_2.data);
+            $display("my_class_2.addr = %0d", cl_2.addr);
         end
 
-        if( ! (cl_2.a < 100) ) begin
-            $error("my_class_2.a >= 100");
-            $display("my_class_2.a = %0d", cl_2.a);
+        if( $countones(cl_2.data) != 1 ) begin
+            $error("my_class_2.data is not power of 2");
+            $display("my_class_2.data = %0d", cl_2.data);
         end
 
-        if( ! (cl_2.b > 50) ) begin
-            $error("my_class_2.b <= 50");
-            $display("my_class_2.b = %0d", cl_2.b);
+        if( !(cl_2.addr < 64) ) begin
+            $error("my_class_2.addr >= 64");
+            $display("my_class_2.addr = %0d", cl_2.addr);
         end
 
         // 3
 
-        if( ! (cl_3.data.size() % 2 == 0) ) begin
+        if( !cl_3.data.size() ) begin
+            $fatal("my_class_3.data.size is zero");
+        end
+
+        if( !(cl_3.data.size() % 2 == 0) ) begin
             $error("my_class_3.data.size() is not even");
             $display("my_class_3.data = %p", cl_3.data);
         end
@@ -123,11 +121,11 @@ initial begin
         end
 
         foreach(cl_3.data[i]) begin
-            if( ! (cl_3.data[i] < 200) ) begin
+            if( !(cl_3.data[i] < 200) ) begin
                 $error("my_class_3.data[%0d] >= 200", i);
                 $display("my_class_3.data[%0d] = %0d", i, cl_3.data[i]);
             end
-            if( (i % 2 == 0) && ( cl_3.data[i] % 2 !=0 )) begin
+            if( (i % 2 == 0) && (cl_3.data[i] % 2 !=0) ) begin
                 $error("my_class_3.data[%0d] is not even, but %0d is even", i, i);
                 $display("my_class_3.data[%0d] = %0d", i, cl_3.data[i]);
             end
@@ -135,15 +133,19 @@ initial begin
 
         // 4
 
+        if( !cl_4.data.size() ) begin
+            $fatal("my_class_4.data.size is zero");
+        end
+
         if ( !(cl_4.data.size() == cl_4.size) ) begin
             $error("size of my_class_4.data != size field");
             $display("my_class_4.data = %p, my_class_4.size = %0d", cl_4.data, cl_4.size);
         end
 
-        if ( cl_4.size == 3) begin
+        if ( cl_4.size == 3 ) begin
             foreach(cl_4.data[i]) begin
                 if( cl_4.data[i] != 0 ) begin
-                    $error("my_class_4.data[i] != 0, my_class_4.but size is 3");
+                    $error("my_class_4.data[i] != 0, but my_class_4.size is 3");
                     $display("my_class_4.data[%0d] = %0d", i, cl_4.data[i]);
                 end
             end
@@ -160,12 +162,13 @@ initial begin
         data_queue.delete();
 
         // 5
+
         if( ! (cl_5.addr[1:0] == 2'b00) ) begin
-            $error("my_class_5.addr is not aligned");
+            $error("my_class_5.addr is not aligned to byte boundary");
             $display("my_class_5.addr = %b", cl_5.addr);
         end
 
-        if( cl_5.req && !(cl_5.data inside {[100:200]})) begin
+        if( cl_5.req && !(cl_5.data inside {[100:200]}) ) begin
             $error("my_class_5.req is 1 but my_class_5.data not inside [100:200]");
             $display("my_class_5.data = %0d", cl_5.data);
         end
@@ -177,7 +180,11 @@ initial begin
 
         // 6
 
-        if( ! (cl_6.tdata.size() == cl_6.tlast.size()) ) begin
+        if( !cl_6.tdata.size() ) begin
+            $fatal("my_class_6.data.size is zero");
+        end
+
+        if( !(cl_6.tdata.size() == cl_6.tlast.size()) ) begin
             $error("my_class_6.tdata and my_class_6.tlast sizes are not equal");
             $display("my_class_6.tdata.size() = %0d, my_class_4.tlast.size() = %0d",
                 cl_6.tdata.size(), cl_6.tlast.size());
@@ -186,15 +193,15 @@ initial begin
             $error("my_class_6.tdata size is not < 33");
             $display("my_class_6.tdata.size() = %0d", cl_6.tdata.size());
         end
-        if( ! (cl_6.tlast.size() < 33) ) begin
+        if( !(cl_6.tlast.size() < 33) ) begin
             $error("my_class_6.tlast size is not < 33");
             $display("my_class_6.tlast.size() = %0d", cl_6.tlast.size());
         end
-        if( ! (cl_6.tdata.size() % 8 == 0) ) begin
+        if( !(cl_6.tdata.size() % 8 == 0) ) begin
             $error("my_class_6.tdata size is not divisible by 8");
             $display("my_class_6.tdata.size() = %0d", cl_6.tdata.size());
         end
-        if( ! (cl_6.tlast.size() % 8 == 0) ) begin
+        if( !(cl_6.tlast.size() % 8 == 0) ) begin
             $error("my_class_6.tlast size is not divisible by 8");
             $display("my_class_6.tlast.size() = %0d", cl_6.tlast.size());
         end
