4032e43d502e195c8fe86b7906226144d9a91b8e
serge06 <chusserge@mail.ru>
2025-04-11 14:54:01 +0300

    Feat(season_3): Добавление скриптов, примеров и заданий 9 занятия

---
+++ season_3/lesson_9/yosys_download.sh
@@ -0,0 +1,2 @@
+wget https://github.com/YosysHQ/oss-cad-suite-build/releases/download/2025-04-11/oss-cad-suite-linux-x64-20250411.tgz
+tar -xvf oss-cad-suite-linux-x64-20250411.tgz
---
+++ season_3/lesson_9/yosys_run.sh
@@ -0,0 +1,2 @@
+export PATH="$(pwd)/oss-cad-suite/bin:$PATH"
+source oss-cad-suite/environment
