19de16ffae1c56cce3baed5aa125e36ac4970c1b
serge06 <chusserge@mail.ru>
2026-02-28 13:16:14 +0300

    Feat(lesson_4): Обновление задания

--- season_4/lesson_1/tasks/04_sum/testbench.sv
+++ season_4/lesson_1/tasks/04_sum/testbench.sv
@@ -19,16 +19,12 @@ module testbench;
 
     // TODO:
     // Определите период тактового сигнала
-    parameter CLK_PERIOD = 10;
+    parameter CLK_PERIOD = ;
 
     // TODO:
     // Cгенерируйте тактовый сигнал
     initial begin
-        clk <= 0;
-        forever begin
-            // Пишите тут.
-            #(CLK_PERIOD/2) clk <= ~clk;
-        end
+
     end
     
     // Генерация сигнала сброса
@@ -57,43 +53,12 @@ module testbench;
     initial begin
         // Входные воздействия опишите здесь.
         // Не забудьте про ожидание сигнала сброса!
-        wait(aresetn);
-        @(posedge clk);
-        A <= 2;
-        B <= 3;
-        @(posedge clk);
-        A <= 20;
-        B <= 30;
-        @(posedge clk);
-        @(posedge clk);
-        A <= 4;
-        B <= 5;
-        @(posedge clk);
-        A <= 40;
-        B <= 50;
-        @(posedge clk);
-        A <= 2;
-        B <= 1;
-        @(posedge clk);
         $stop();
     end
 
     initial begin
         // Проверки опишите здесь.
         // Не забудьте про ожидание сигнала сброса!
-        wait(aresetn);
-        @(posedge clk);
-        @(posedge clk);
-        @(posedge clk);
-        if( C !== 5 ) $error("BAD");
-        @(posedge clk);
-        if( C !== 50 ) $error("BAD");
-        @(posedge clk);
-        if( C !== 50 ) $error("BAD");
-        @(posedge clk);
-        if( C !== 9 ) $error("BAD");
-        @(posedge clk);
-        if( C !== 90 ) $error("BAD");
     end
 
 endmodule
